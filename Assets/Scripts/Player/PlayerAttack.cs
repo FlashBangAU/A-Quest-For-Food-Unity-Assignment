d635cd09448c82419bf42b555b1fd9e0cf741f97
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-08-05 10:43:04 +1000

    Visual Timer Added

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -13,15 +13,18 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] int damage;
 
     [SerializeField] private Animator attackAnimation;
+    public Timer timer;
 
     // Update is called once per frame
     void Update()
     {
-        if(timeBtwAttack <-0)
+        if(timeBtwAttack < 0)
         {
+            timer.StopTimer();
             if (Input.GetKeyDown(KeyCode.P))
             {
                 timeBtwAttack = startTimeBtwAttack;
+                timer.StartTimer();
                 attackAnimation.Play("AttackAnimation");
                 Collider2D[] eniemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
                 for (int i = 0; i < eniemiesToDamage.Length; i++)
@@ -29,7 +32,8 @@ public class PlayerAttack : MonoBehaviour
                     eniemiesToDamage[i].GetComponent<EnemyHealth>().TakeDamage(damage);
                 }
             }
-        } else
+            
+        }else
         {
             timeBtwAttack -= Time.deltaTime;
         }
