37473bda1e3484e0f1faf59334551b3ca6a6961c
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-10-03 21:00:51 +1000

    Revert "merge from sound branch"
    This reverts commit c5636bdb25a0280be1b1f522a8853bc1246b7b3b, reversing
    changes made to 4ad1f71cfc405993ebe4647983ae3916e540a8df.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -35,8 +35,6 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isKnockedBack = false; // Flag for knockback state
 
-    bool onDescent =false;
-
     Rigidbody2D rb;
 
     AudioManager audioManager;
@@ -58,7 +56,6 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         jumpWait = jumpWait * Time.deltaTime;
-
 
 
         // Logging horizontal input
@@ -163,11 +160,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             // Player Left Contact Jump
-<<<<<<< HEAD
             if (Input.GetKey(KeyCode.W) && jumpLeft.canJump && !jumpedLeft && onDescent || Input.GetKeyDown(KeyCode.W) && jumpLeft.canJump && !jumpedLeft)
-=======
-            if (Input.GetKey(KeyCode.W) && jumpLeft.canJump && !jumpedLeft && onDescent || Input.GetKeyDown(KeyCode.W))
->>>>>>> sound-branch
             {
                 if (Input.GetKey(KeyCode.A))
                 {
@@ -187,11 +180,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             // Player Right Contact Jump
-<<<<<<< HEAD
             if (Input.GetKey(KeyCode.W) && jumpRight.canJump && !jumpedRight && onDescent || Input.GetKeyDown(KeyCode.W) && jumpRight.canJump && !jumpedRight)
-=======
-            if (Input.GetKey(KeyCode.W) && jumpRight.canJump && !jumpedRight && onDescent || Input.GetKeyDown(KeyCode.W))
->>>>>>> sound-branch
             {
                 if (Input.GetKey(KeyCode.A))
                 {
