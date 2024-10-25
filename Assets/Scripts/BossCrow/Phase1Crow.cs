bd82749b8d534c2799ed4f9bf0ff2bd1b09abcae
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-10-25 20:35:39 +1000

    Revert "Boss phase 1 movement"
    This reverts commit 43dcd83ae77058991ddf66b5efd20ae4f311a193.

--- Assets/Scripts/BossCrow/Phase1Crow.cs
+++ Assets/Scripts/BossCrow/Phase1Crow.cs
@@ -8,6 +8,7 @@ public class Phase1Crow : MonoBehaviour
     public Rigidbody2D rb;
     private GameObject player;
     public GameObject hitBoxPhase1;
+    public AttackPlayerBoss attackPlayerBoss;
     [SerializeField] float attackRange;
 
     private float xBtw;
@@ -16,21 +17,37 @@ public class Phase1Crow : MonoBehaviour
     [SerializeField] float jumpHeight;
     [SerializeField] float jumpDistance;
 
-    [SerializeField] bool hoppingMode;
-    [SerializeField] bool peckMode;
+    bool runAway;
+    bool hoppingMode = true;
+    bool peckMode;
 
-    [SerializeField] bool peckCycle;
     [SerializeField] float maxTimeBtwPeck;
     [SerializeField] float minTimeBtwPeck;
-    [SerializeField] private float timeBtwPeck;
-    [SerializeField] private float nextPeck;
-    [SerializeField] private int peckCounter;
+    private float timeBtwPeck;
+    private float nextPeck;
+    private int peckCounter;
 
-    [SerializeField] bool onGround;
+    private float tStuck;
+    public float timeStuck;
+
+    bool onGround;
     private bool hoppingRight;
+    private bool weakPointActive = false;
+
+    [SerializeField] private Animator animatior;
+
+    private AudioManager audioManager; // Reference for AudioManager
+
+    // Used to connect the AudioManager reference to the existing AudioManager object
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        hitBoxPhase1.SetActive(false);
     }
 
     // Update is called once per frame
@@ -46,60 +63,81 @@ public class Phase1Crow : MonoBehaviour
         }
 
         timeBtwPeck += Time.deltaTime;
+        tStuck += Time.deltaTime;
 
 
-        if(pc.phase1 == true)
+        if (pc.phase1 == true)
         {
-            if (hoppingMode && !peckCycle)
+            if (!onGround)
             {
+                animatior.Play("hopAnimation");
+            }
+
+            //bird runs to the right of screen
+            if (runAway && onGround)
+            {
+                //play hop animation
+                rb.velocity = new Vector2(jumpDistance, jumpHeight);
+                audioManager.PlaySFX(audioManager.bossJump);
+                onGround = false;
+                if (tStuck > 3f)
+                {
+                    runAway = false;
+                    hoppingMode = true;
+                }
+            }
+            else if (hoppingMode)
+            {
+                //player hopping towards player
                 if (onGround && hoppingRight)
                 {
                     //play hop animation
                     rb.velocity = new Vector2(jumpDistance, jumpHeight);
+                    audioManager.PlaySFX(audioManager.bossJump);
                     onGround = false;
                 }
                 else if (onGround && !hoppingRight)
                 {
                     //play hop anmiation
                     rb.velocity = new Vector2(-jumpDistance, jumpHeight);
+                    audioManager.PlaySFX(audioManager.bossJump);
                     onGround = false;
                 }
 
+                //gets range away from player for peck
                 xBtw = player.transform.position.x - hitBoxPhase1.transform.position.x;
                 if(xBtw < 0)
                 {
                     xBtw *= -1;
                 }
+                //gets range away from player for peck
                 yBtw = player.transform.position.y - hitBoxPhase1.transform.position.y;
                 if (yBtw < 0)
                 {
                     yBtw *= -1;
                 }
 
+                //checks if player is in range for peck
                 if (xBtw < attackRange && yBtw < attackRange)
                 {
                     peckMode = true;
                     hoppingMode = false;
-                    peckCycle = true;
                     timeBtwPeck = 0f;
                     nextPeck = Random.Range(minTimeBtwPeck, maxTimeBtwPeck);
                 }
             }
             else if (peckMode && onGround)
-            {
-                //play peck animation
-                
+            {                
                 if(nextPeck <= timeBtwPeck && peckCounter < 3)
                 {
                     Peck();
-                    Debug.Log("Pecked at Player");
+                    //Debug.Log("Pecked at Player");
 
                     nextPeck = Random.Range(minTimeBtwPeck, maxTimeBtwPeck);
                     timeBtwPeck = 0f;
                     peckCounter++;
-                } else if (peckCounter == 3)
+                } else if (peckCounter == 3 && nextPeck <= timeBtwPeck)
                 {
-                    peckCounter = 0;
                     Stuck();
                 }
                 
@@ -107,23 +145,53 @@ public class Phase1Crow : MonoBehaviour
         }
     }
 
+    //will damage player
+    private void Peck()
+    {
+        //play peck animation
+        //Debug.Log("Peck action is played");
+        attackPlayerBoss.PlayerGotHit();
+        animatior.Play("peckAnimation");
+        audioManager.PlaySFX(audioManager.peckGround);
+    }
+
+    //will make boss vulnrable for a period of time
+    private void Stuck()
+    {
+        if (weakPointActive == false)
+        {
+            Peck();
+            tStuck = 0f;
+            hitBoxPhase1.SetActive(true);
+            weakPointActive = true;
+        }
+        animatior.Play("peckAnimation");
+
+        if (tStuck > timeStuck)
+        {
+            unStuck();
+        }
+    }
+
+    //used when boss is damaged in phase 1 hitbox
+    public void unStuck()
+    {
+        hoppingMode = true;
+        peckMode = false;
+        hitBoxPhase1.SetActive(false);
+        weakPointActive = false;
+        runAway = true;
+        peckCounter = 0;
+        tStuck = 0f;
+        audioManager.PlaySFX(audioManager.bossCall);
+    }
+
+    //checks if boss has contact with ground
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground") && !onGround)
         {
             onGround = true;
         }
-    }
-
-    private void Peck()
-    {
-
-    }
-
-    private void Stuck()
-    {
-        peckMode = false;
-        peckCycle = false;
-        hoppingMode = true;
     }
 }
